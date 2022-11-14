cefadbf98b504304f0a3f7d5c19cb6512e8ee0c3
Sera Zenginler <serazenginler@outlook.com>
2022-11-14 22:09:17 +0300

    open and close nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,57 @@
-import DensitySmallIcon from '@mui/icons-material/DensitySmall';
+import { useState } from "react";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import DensitySmallIcon from "@mui/icons-material/DensitySmall";
+import CloseIcon from "@mui/icons-material/Close";
+import styles from "./styles.module.scss";
 
 const Navbar = () => {
-  return (
-    <nav>
-        <DensitySmallIcon />
-    </nav>
-  )
-}
+  const links = [
+    { text: "Explore", link: "/" },
+    {
+      text: "Stats",
+      link: "/stats",
+    },
+    {
+      text: "Drops",
+      link: "/drops",
+    },
+    {
+      text: "Activity",
+      link: "/activity",
+    },
+  ];
 
-export default Navbar
+  const [visible, setVisible] = useState(false);
+
+  const open = () => setVisible(true);
+
+  const close = () => setVisible(false);
+
+  return (
+    <nav className={`center ${styles.nav}`}>
+      <Link to="/">
+        <Logo />
+      </Link>
+
+      <DensitySmallIcon className="pointer" onClick={open} />
+
+      <ul className={`${styles.navLinks} ${visible ? styles.visible : ""}`}>
+        <li
+          className={`pointer ${styles.closeIcon} ${
+            visible ? styles.visible : ""
+          }`}
+        >
+          <CloseIcon onClick={close} />
+        </li>
+        {links.map((link, index) => (
+          <li key={index} onClick={close}>
+            <Link to={link.link}>{link.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
