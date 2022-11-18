c81e9eaee0575e95969c48dfca519c68e7150722
Sera Zenginler <serazenginler@outlook.com>
2022-11-18 23:40:06 +0300

    add desktop hero styles

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,25 +8,32 @@ const Hero = () => {
   const handleChange = ({ target }) => setSearch(target.value);
 
   return (
-    <section className={`center ${styles.hero}`}>
-      <h1>Discover, collect, and sell extraordinary NFTs</h1>
-      <p>
-        Our marketplace is the world's first and largest NFT market for
-        independent creators worldwide
-      </p>
-      <div className={styles.inputContainer}>
-        <label
-          htmlFor="search"
-          className={search === "" ? styles.empty : styles.full}
-        >
-          Type to search
-        </label>
-        <input type="text" id="search" value={search} onChange={handleChange} />
-        <button>
-          <SearchIcon />
-        </button>
-      </div>
-    </section>
+    <header className={`center ${styles.hero}`}>
+      <article className="center">
+        <h1>Discover, collect, and sell extraordinary NFTs</h1>
+        <p>
+          Our marketplace is the world's first and largest NFT market for
+          independent creators worldwide.
+        </p>
+        <div className={styles.inputContainer}>
+          <label
+            htmlFor="search"
+            className={search === "" ? styles.empty : styles.full}
+          >
+            Type to search
+          </label>
+          <input
+            type="text"
+            id="search"
+            value={search}
+            onChange={handleChange}
+          />
+          <button>
+            <SearchIcon />
+          </button>
+        </div>
+      </article>
+    </header>
   );
 };
 
