bb8823ca87dbbc754a905fae1d2d1c15c7abd093
Sera Zenginler <serazenginler@outlook.com>
2022-11-20 23:44:35 +0300

    add top seller mobile

--- src/components/TopSellers/index.jsx
+++ src/components/TopSellers/index.jsx
@@ -1,10 +1,31 @@
 import styles from "./styles.module.scss";
 import { top_sellers } from "../../data";
-const TopSellers = () => {
-    console.log(top_sellers)
-  return (
-    <section><img src={top_sellers[0].img} alt="" /></section>
-  )
-}
 
-export default TopSellers
+const TopSellerContainer = ({ seller }) => {
+  return (
+    <section className={`center ${styles.col}`}>
+      <p>{seller.id}</p>
+      <img src={seller.img} alt={seller.name} />
+      <article>
+        <h3>{seller.name}</h3>
+        <p>{`$${seller.value.toFixed(2)}`}</p>
+      </article>
+      <p className={styles.percent}>{`+${seller.percent}%`}</p>
+    </section>
+  );
+};
+
+const TopSellers = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h2>Top selling creators</h2>
+      <div className={styles.container}>
+        {top_sellers.map((seller) => (
+          <TopSellerContainer seller={seller} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TopSellers;
--- src/data/index.js
+++ src/data/index.js
@@ -8,36 +8,42 @@ export const data = [
 
 export const top_sellers = [
   {
+    id: 1,
     name: "Bored Ape Yacht Club",
     value: 10450,
     percent: 12.5,
     img: "/images/top_sellers/bayc.webp",
   },
   {
+    id: 2,
     name: "Bored Ape Chemistry Club",
     value: 5344.13,
     percent: 6.3,
     img: "/images/top_sellers/bacc.png",
   },
   {
-    name: "Chromie Squiggle by Snowfro",
+    id: 3,
+    name: "Chromie Squiggle",
     value: 19320,
     percent: 18.4,
     img: "/images/top_sellers/chromie.png",
   },
   {
+    id: 4,
     name: "Bored Ape Kennel Club",
     value: 4579.4,
     percent: 13.5,
     img: "/images/top_sellers/bakc.png",
   },
   {
+    id: 5,
     name: "Worldwide Webb Land",
     value: 13457,
     percent: 7.25,
     img: "/images/top_sellers/wwl.png",
   },
   {
+    id: 6,
     name: "Brick-and-Mortar Travails",
     value: 3355.2,
     percent: 9.3,
