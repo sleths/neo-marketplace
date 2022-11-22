e971d0e2cacb6ab396148118a9e2c4ec5677cad6
Sera Zenginler <serazenginler@outlook.com>
2022-11-22 22:25:39 +0300

    add first nft data

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,171 @@
 export const data = [
   {
     user: {
-      username: "sera",
+      username: "Andrea Piacquadio",
+      img: "./images/user/user_andrea.webp",
+      followers: 102,
+      following: 78,
     },
+    nfts: [
+      {
+        name: "Election Season",
+        collection: "Irene Sotelo",
+        description: "Description",
+        likes: 16,
+        price: 0.5,
+        tags: [""],
+        img: "./images/nft/nft_one.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Hasibullah Zhowandai",
+      img: "./images/user/user_hasibullah.jpeg",
+      followers: 210,
+      following: 350,
+    },
+    nfts: [
+      {
+        name: "Wall of Season",
+        collection: "Irene Sotelo",
+        description: "Description",
+        likes: 35,
+        price: 0.5,
+        tags: [""],
+        img: "./images/nft/nft_two.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Dazzle Jam",
+      img: "./images/user/user_dazzle.jpeg",
+      followers: 10,
+      following: 1,
+    },
+    nfts: [
+      {
+        name: "Lorr’s Ad Issues",
+        collection: "Irene Sotelo",
+        description: "Description",
+        likes: 30,
+        price: 1,
+        tags: [""],
+        img: "./images/nft/nft_three.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Danang Wicaksono",
+      img: "./images/user/user_danang.jpeg",
+      followers: 2000,
+      following: 50,
+    },
+    nfts: [
+      {
+        name: "You Fire",
+        collection: "Irene Sotelo",
+        description: "Description",
+        likes: 20,
+        price: 0.05,
+        tags: [""],
+        img: "./images/nft/nft_four.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Hamann La",
+      img: "./images/user/user_hamann.jpeg",
+      followers: 33,
+      following: 46,
+    },
+    nfts: [
+      {
+        name: "Shib Toast",
+        collection: "Diane Lansdowne",
+        description: "Description",
+        likes: 100,
+        price: 0.15,
+        tags: [""],
+        img: "./images/nft/nft_five.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Godisable Jacob",
+      img: "./images/user/user_godisable.jpeg",
+      followers: 5369,
+      following: 10,
+    },
+    nfts: [
+      {
+        name: "Applice",
+        collection: "Diane Lansdowne",
+        description: "Description",
+        likes: 1273,
+        price: 2.5,
+        tags: [""],
+        img: "./images/nft/nft_six.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Noelle Otto",
+      img: "./images/user/user_noelle.webp",
+      followers: 235,
+      following: 190,
+    },
+    nfts: [
+      {
+        name: "Cubic Love",
+        collection: "Diane Lansdowne",
+        description: "Description",
+        likes: 13,
+        price: 0.135,
+        tags: [""],
+        img: "./images/nft/nft_seven.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
+  },
+  {
+    user: {
+      username: "Royal Anwar",
+      img: "./images/user/user_royal.jpeg",
+      followers: 87,
+      following: 92,
+    },
+    nfts: [
+      {
+        name: "Insider Open Door",
+        collection: "Diane Lansdowne",
+        description: "Description",
+        likes: 45,
+        price: 1.5,
+        tags: [""],
+        img: "./images/nft/nft_eight.jpg",
+        recommended_creators: true,
+        todays_pick: false,
+      }
+    ]
   },
 ];
 
