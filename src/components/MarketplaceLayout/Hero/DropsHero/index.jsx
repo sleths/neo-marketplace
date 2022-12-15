38606044494bb24a0a236aa4ca5bd09ed1aa380d
Sera Zenginler <serazenginler@outlook.com>
2022-12-15 22:59:14 +0300

    drop page hero

--- src/components/MarketplaceLayout/Hero/DropsHero/index.jsx
+++ src/components/MarketplaceLayout/Hero/DropsHero/index.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+import LiveTag from "../../../Tag/LiveTag";
 import styles from "./styles.module.scss";
 
 const DropsHero = () => {
   return (
     <header className={styles.hero}>
-      <article className="center">
-        <p>Live Now</p>
+      <article className="center max-width">
+        <LiveTag />
         <h1>Invisible Friends</h1>
-        <strong>Ends in: 18h 54m 29s</strong>
-        <button>View Drop Details</button>
+        <strong className={styles.counter}>Ends in: 18h 54m 29s</strong>
+        <Link to="#" className={styles.cta}>
+          View Drop Details
+        </Link>
       </article>
     </header>
   );
---
+++ src/components/Tag/LiveTag/index.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+const LiveTag = () => {
+  return <p className={styles.tag}>Live Now</p>;
+};
+
+export default LiveTag;
